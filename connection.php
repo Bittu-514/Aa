9c387b72b093aa0ad71628565371900a171c8add
Bittu-514 <94109128+Bittu-514@users.noreply.github.com>
2022-03-03 19:36:40 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php  
      	$servername = "34.203.204.238";
-	$username = "root";  
-       	$password = "root";  
+	$username = "admin";  
+       	$password = "Bbbittu";  
        	$conn = mysql_connect ($servername , $username , $password) or die("unable to connect to host");  
 	$sql = mysql_select_db ('travel',$conn) or die("unable to connect to database");	
 ?>
